578dbbb3511404b113e3fdea7a21cea9647c3aa8
Dylan Yuan <l>
2020-03-31 21:25:18 +0800

    setup resume-generator server

---
+++ resume-generator-server/src/main/java/com/icodingman/resume/ResumeGeneratorServerApplication.java
@@ -0,0 +1,13 @@
+package com.icodingman.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeGeneratorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeGeneratorServerApplication.class, args);
+    }
+
+}
