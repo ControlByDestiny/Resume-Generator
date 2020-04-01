57441815bb87e13fd816a20e1c8899c8961f757c
Dylan Yuan <l>
2020-04-01 21:15:06 +0800

    add ci

--- resume-generator-server/src/main/java/com/icodingman/resume/ResumeGeneratorServerApplication.java
+++ resume-generator-server/src/main/java/com/icodingman/resume/ResumeGeneratorServerApplication.java
@@ -2,8 +2,10 @@ package com.icodingman.resume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan( basePackages = {"com.icodingman.resume.controllers"})
 public class ResumeGeneratorServerApplication {
 
     public static void main(String[] args) {
